019813f58528fa20fbe6ee2d5109fcf80b802671
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2023-12-08 12:48:03 +0100

    Add project files.

---
+++ LuappDev/LuappDev.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LuappDev
+{
+	TEST_CLASS(LuappDev)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
