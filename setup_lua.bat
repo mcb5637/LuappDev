cf63ee175ba23d09cdcf70d97bb8f33e4c002f50
mcb <mgrbo@web.de>
2026-04-18 14:18:47 +0200

    more win ci fixes

--- setup_lua.bat
+++ setup_lua.bat
@@ -1,23 +1,4 @@
 
-
-:download_lua
-setlocal
-set link=%~1
-set out="./LuappDev/%~2"
-set tmp="./lua.zip"
-if exist "%out%/" (
-    echo "%~2 already exists"
-) else (
-    curl -o "%tmp%" -L "%link%"
-    mkdir "%out%"
-    tar -xf "%tmp%" -C "%out%"
-    del "%tmp%"
-    move /Y "%out%/include/*" "%out%"
-    del "%out%/include/"
-)
-endlocal
-EXIT /B 0
-
 call :download_lua "https://sourceforge.net/projects/luabinaries/files/5.5.0/Windows%%20Libraries/Static/lua-5.5.0_Win64_vc17_lib.zip/download" "lua55"
 call :download_lua "https://sourceforge.net/projects/luabinaries/files/5.4.8/Windows%%20Libraries/Static/lua-5.4.8_Win64_vc17_lib.zip/download" "lua54"
 call :download_lua "https://sourceforge.net/projects/luabinaries/files/5.3.6/Windows%%20Libraries/Static/lua-5.3.6_Win64_vc17_lib.zip/download" "lua53"
@@ -39,3 +20,23 @@ if exist "./LuappDev/luajit" (
     copy "./luajit_src/src/luajit.h" "./LuappDev/luajit"
     copy "./luajit_src/src/luaconf.h" "./LuappDev/luajit"
 )
+
+EXIT /B %ERRORLEVEL%
+
+:download_lua
+setlocal
+set link=%~1
+set out="./LuappDev/%~2"
+set tmp="./lua.zip"
+if exist "%out%/" (
+    echo "%~2 already exists"
+) else (
+    curl -o "%tmp%" -L "%link%"
+    mkdir "%out%"
+    tar -xf "%tmp%" -C "%out%"
+    del "%tmp%"
+    move /Y "%out%/include/*" "%out%"
+    del "%out%/include/"
+)
+endlocal
+EXIT /B 0
